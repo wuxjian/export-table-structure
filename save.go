3e03f947abd220dd4149ecbd78cdaa7a6b79d0e3
wuxjian <wu_xjian@163.com>
2021-02-25 15:30:40 +0800

    save txt

--- save.go
+++ save.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"github.com/tealeg/xlsx/v3"
+	"io"
 	"log"
+	"os"
 )
 
-func Save(c <-chan *TableInfo, finish chan<- struct{})  {
+func Save(c <-chan *TableInfo, finish chan<- struct{}) {
 	go func() {
-		wb := xlsx.NewFile()
-
-		for t := range c{
-			save(t, wb)
+		//wb := xlsx.NewFile()
+		file, err := os.OpenFile("表结构.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 066)
+		if err != nil {
+			panic(err)
 		}
-		wb.Save("./表结构.xlsx")
+		defer file.Close()
+
+		for t := range c {
+			saveTxt(t, file)
+		}
+		//wb.Save("./表结构.xlsx")
 		finish <- struct{}{}
 	}()
+}
+
+func saveTxt(t *TableInfo, w io.Writer) {
+	tableName := t.TableName
+	if t.TableComment != "" {
+		tableName += "(" + t.TableComment + ")"
+	}
+
+	io.WriteString(w, "表名:"+tableName+"\r\n")
+
+	io.WriteString(w, "字段名称\t数据类型\t必填\t注释\r\n")
+	for _, column := range t.Columns {
+		io.WriteString(w, column.ColumnName+"\t"+column.ColumnType+"\t"+column.Required+"\t"+column.Comment+"\r\n")
+	}
 }
 
 func save(t *TableInfo, wb *xlsx.File) {
@@ -24,8 +45,6 @@ func save(t *TableInfo, wb *xlsx.File) {
 	}
 
 	sheet, err := wb.AddSheet(sheetName)
-
-
 
 	if err != nil {
 		log.Fatalf("add sheet[%s] fail:%v\n", t.TableName, err)
